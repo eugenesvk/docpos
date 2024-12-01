ae976c915281a02e611f8bd0a7d2663271a621d5
eugenesvk <eugenesvk@users.noreply.github.com>
2024-12-01 17:57:19 +0700

    test: add

---
+++ tests/expansion/argdocpos_only3a.expanded.rs
@@ -0,0 +1,7 @@
+use roxygen::*;
+/// fn_Pre
+/// fn_Pos
+/// **Parameters**:
+///
+/// * `par1`: fn→par1
+pub fn tst(par1: f32) {}
---
+++ tests/expansion/argdocpos_only3a.rs
@@ -0,0 +1,8 @@
+use roxygen::*;
+
+#[argdocpos]
+/// fn_Pre
+pub fn tst( /// fn_Pos
+  ///!          fn→par1
+  par1: f32,
+) {}
