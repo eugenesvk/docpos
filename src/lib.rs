a708dc360411b2f976f4ccdbe0255830d4571924
eugenesvk <eugenesvk@users.noreply.github.com>
2024-12-01 13:22:59 +0700

    add silencers for various syntax warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions),allow(non_snake_case,non_upper_case_globals,non_camel_case_types))]
+#![cfg_attr(    debug_assertions ,allow(non_snake_case,non_upper_case_globals,non_camel_case_types,unused_imports,unused_mut,unused_variables,dead_code,unused_assignments,unused_macros))]
 #![doc= include_str!("../Readme.md")]
 //! ## Documenting Generics
 //! Generic parameters can be documented with doc comments just as the arguments
