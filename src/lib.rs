3b945dab59e31dff6ee89aaa719531f742bd2be4
eugenesvk <eugenesvk@users.noreply.github.com>
2024-12-01 13:26:21 +0700

    condense

--- src/lib.rs
+++ src/lib.rs
@@ -121,59 +121,36 @@ pub fn argdocpos(
     let mut function: ItemFn = parse_macro_input!(item as ItemFn);
 
     try2!(function.attrs.iter_mut().try_for_each(|attr| {
-        if is_argdocpos_main(attr) {
-            Err(syn::Error::new_spanned(
-                attr,
-                "Duplicate attribute. This attribute must only appear once.",
-            ))
-        } else {
-            Ok(())
-        }
-    }));
+        if is_argdocpos_main(attr) {Err(syn::Error::new_spanned(attr,"Duplicate attribute. This attribute must only appear once.",))
+        } else                     {Ok(())}}));
 
-    // extrac the doc attributes on the function itself
-    let function_docs = try2!(extract_fn_doc_attrs(&mut function.attrs));
+    let function_docs = try2!(extract_fn_doc_attrs(&mut function.attrs)); // extrac the doc attributes on the function itself
 
-    let (doc_params_to_fn, documented_params) = try2!(extract_documented_parameters_shift_up(
-        function.sig.inputs.iter_mut()
-    ));
+    let (doc_params_to_fn, documented_params) = try2!(extract_documented_parameters_shift_up(function.sig.inputs.iter_mut()));
     let maybe_empty_doc_par_to_fn: Vec<Attribute> = doc_params_to_fn.unwrap_or_else(|| vec![]);
-
-    let documented_generics = try2!(extract_documented_generics(&mut function.sig.generics));
-
-    let has_documented_params = !documented_params.is_empty();
+    let documented_generics     = try2!(extract_documented_generics(&mut function.sig.generics));
+    let has_documented_params   = !documented_params  .is_empty();
     let has_documented_generics = !documented_generics.is_empty();
 
     if !has_documented_params && !has_documented_generics {
-        return syn::Error::new_spanned(
-            function.sig.ident,
-            "Function has no documented parameters or generics.\nDocument at least one function parameter or generic.",
-        )
-        .into_compile_error()
-        .into();
-    }
+        return syn::Error::new_spanned(function.sig.ident,"Function has no documented parameters or generics.\nDocument at least one function parameter or generic.",)
+        .into_compile_error().into();}
 
     let parameter_doc_block = make_doc_block("Parameters", documented_params);
-    let generics_doc_block = make_doc_block("Generics", documented_generics);
+    let generics_doc_block  = make_doc_block("Generics", documented_generics);
 
     let docs_before = function_docs.before_args_section;
-    let docs_after = function_docs.after_args_section;
-    let maybe_empty_doc_line = if !docs_after.is_empty() {
-        Some(quote! {#[doc=""]})
-    } else {
-        None
-    };
+    let docs_after  = function_docs.after_args_section;
+    let maybe_empty_doc_line = if !docs_after.is_empty() {Some(quote! {#[doc=""]})
+    } else                                               {None};
 
-    quote! {
-        #(#docs_before)*
-        #(#maybe_empty_doc_par_to_fn)*
+    quote! {#(#docs_before)* #(#maybe_empty_doc_par_to_fn)*
         #parameter_doc_block
         #generics_doc_block
         #maybe_empty_doc_line
         #(#docs_after)*
         #function
-    }
-    .into()
+    }.into()
 }
 
 // this is to expose the helper attribute #[arguments_section].
