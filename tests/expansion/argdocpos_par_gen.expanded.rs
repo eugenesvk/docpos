76894a20f85ccd5b2638504cda647aa052c7055b
eugenesvk <eugenesvk@users.noreply.github.com>
2024-12-01 19:33:43 +0700

    test: add

---
+++ tests/expansion/argdocpos_par_gen.expanded.rs
@@ -0,0 +1,17 @@
+use roxygen::*;
+/// fnPre line 1.
+/// fnPre line 2.
+///
+/// **Parameters**:
+/// * `par2`: par2 (par1 is not documented)
+///
+/// **Generics**:
+/// * `a`: pre-a-lifetime (preserved)
+///        pos-a-lifetime
+/// * `S`: documentation for parameter S
+///        spans multiple lines
+/// * `T`: T self
+/// * `N`: T→N const generic
+///
+/// fnPre line 3 after the parameters/generics section
+fn with_lifetimes<'a,S,T,const N>(par1: i32, par2: String, _undocumented: i32) -> bool {par2.len() > par1 as usize}
---
+++ tests/expansion/argdocpos_par_gen.rs
@@ -0,0 +1,18 @@
+use roxygen::*;
+
+#[argdocpos]
+/// fnPre line 1.
+/// fnPre line 2.
+#[parameters_section]
+/// fnPre line 3 after the parameters/generics section
+fn with_lifetimes</// pre-a-lifetime (preserved)
+  'a 	,/// pos-a-lifetime
+  S  	,/// documentation for parameter S
+  ///	     spans multiple lines
+  T  	,/// T self
+       ///! T→N const generic
+  const N: usize,>(/// fnInner
+  par1: u32,
+  par2: String, /// par2 (par1 is not documented)
+  _undocumented: i32,
+) -> bool {par2.len() > par1 as usize}
