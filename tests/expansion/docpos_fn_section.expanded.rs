9139d078b50a1b1d1539cb27b9ca0eaf49ca5b15
eugenesvk <eugenesvk@users.noreply.github.com>
2024-12-02 13:45:10 +0700

    test: update

---
+++ tests/expansion/docpos_fn_section.expanded.rs
@@ -0,0 +1,25 @@
+use roxygen::*;
+/// fnOuter line 1
+/// fnOuter line 2
+///
+/// **Parameters**:
+///
+/// * `p1`: p1→p1 line 1
+/// * `p2`: p2→p2 line 1
+/// p2→p2 line 2
+///
+/// fnInnter after the arguments section
+pub fn foo(par1: f32, par2: f32) {}
+
+/// fnOuter line 1
+/// fnOuter line 2
+///
+/// **Parameters**:
+///
+/// * `p1`: p1→p1 line 1
+/// * `p2`: p2→p2 line 1
+/// p2→p2 line 2
+///
+/// fnInnter after the arguments section
+pub fn foo2(par1: f32, par2: f32) {}
+
---
+++ tests/expansion/docpos_fn_section.rs
@@ -0,0 +1,23 @@
+use roxygen::*;
+
+#[docpos]
+/// fnOuter line 1
+/// fnOuter line 2
+#[parameters_section]
+fn foo( /// fnInnter after the arguments section
+  p1           	: u32   	,/// p1→p1 line 1
+  p2           	: String	,/// p2→p2 line 1
+               	        	 /// p2→p2 line 2
+  _undocumented	: i32   	,
+) -> bool {p2.len() > p1 as usize}
+
+#[docpos("fn")]
+/// fnOuter line 1
+/// fnOuter line 2
+#[parameters_section]
+fn foo2( /// fnInnter after the arguments section
+  p1           	: u32   	,/// p1→p1 line 1
+  p2           	: String	,/// p2→p2 line 1
+               	        	 /// p2→p2 line 2
+  _undocumented	: i32   	,
+) -> bool {p2.len() > p1 as usize}
