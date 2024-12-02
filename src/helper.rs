cfff4db8ee1b04f44cb051cd8b5335c19c74dcb8
eugenesvk <eugenesvk@users.noreply.github.com>
2024-12-02 19:15:54 +0700

    update macro path check

--- src/helper.rs
+++ src/helper.rs
@@ -1,4 +1,14 @@
 use crate::Attribute;
 
+const DOCPOS_CRATE: &str = "docpos";
+const DOCPOS_MACRO: &str = "docpos";
+
 /// check whether an attribute is the raw #[argdocpos] main attribute.
-#[inline(always)]pub fn is_docpos_main(attr: &Attribute) -> bool {attr.path().is_ident("docpos")}
+#[inline(always)]pub fn is_docpos_main(attr: &Attribute) -> bool {
+  let path = attr.path();
+  if path.is_ident(DOCPOS_MACRO) {true
+  } else { path.segments.len() == 2 // checks for (::)docpos::docpos
+    &&     path.segments[0].ident == DOCPOS_CRATE
+    &&     path.segments[1].ident == DOCPOS_MACRO
+  }
+}
