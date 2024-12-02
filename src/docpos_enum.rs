bf67b5ff53f7d4385ec529886cf42e55b2449690
eugenesvk <eugenesvk@users.noreply.github.com>
2024-12-02 16:57:52 +0700

    enum: fix names

--- src/docpos_enum.rs
+++ src/docpos_enum.rs
@@ -12,18 +12,18 @@ pub fn docpos_enum(mut enm:ItemEnum) -> proc_macro::TokenStream {
     if is_docpos_main(attr) {Err(syn::Error::new_spanned(attr,"Duplicate attribute. This attribute must only appear once.",))
     } else                  {Ok(())}}));
 
-  let enum_docs = try2!(extract_struct_doc_attrs(&mut enm.attrs)); // extrac the doc attributes on the struct itself
+  let enum_docs = try2!(extract_struct_doc_attrs(&mut enm.attrs)); // extrac the doc attributes on the enum itself
 
-  let (doc_params_to_enm, doc_fields) = try2!(extract_doc_variants_shift_up(enm.variants.iter_mut()));
-  let maybe_empty_doc_par_to_fn: Vec<Attribute> = doc_params_to_enm.unwrap_or_else(|| vec![]);
+  let (doc_params_to_enm, doc_variants) = try2!(extract_doc_variants_shift_up(enm.variants.iter_mut()));
+  let maybe_empty_doc_par_to_enum: Vec<Attribute> = doc_params_to_enm.unwrap_or_else(|| vec![]);
   // let documented_generics     = try2!(extract_documented_generics_shift_up(&mut enm.generics));
-  let has_doc_fields   = !doc_fields  .is_empty();
+  let has_doc_variants   = !doc_variants  .is_empty();
   // let has_documented_generics = !documented_generics.is_empty();
 
-  // if !has_doc_fields && !has_documented_generics {
-  if !has_doc_fields {return syn::Error::new_spanned(enm.ident,"Enum has no documented variants or generics. Document at least one.",).into_compile_error().into();}
+  // if !has_doc_variants && !has_documented_generics {
+  if !has_doc_variants {return syn::Error::new_spanned(enm.ident,"Enum has no documented variants or generics. Document at least one.",).into_compile_error().into();}
 
-  let parameter_doc_block = make_doc_block("Variants", doc_fields);
+  let parameter_doc_block = make_doc_block("Variants", doc_variants);
   // let generics_doc_block  = make_doc_block("Generics", documented_generics);
 
   let docs_before = enum_docs.before_args_section;
@@ -31,7 +31,7 @@ pub fn docpos_enum(mut enm:ItemEnum) -> proc_macro::TokenStream {
   let maybe_empty_doc_line = if !docs_after.is_empty() {Some(quote! {#[doc=""]})
   } else                                               {None};
 
-  quote! {#(#docs_before)* #(#maybe_empty_doc_par_to_fn)*
+  quote! {#(#docs_before)* #(#maybe_empty_doc_par_to_enum)*
     #parameter_doc_block
   //     #generics_doc_block
     #maybe_empty_doc_line
