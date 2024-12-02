5223c3d2349c2b4170578ea440f7fceed7e902ad
eugenesvk <eugenesvk@users.noreply.github.com>
2024-12-02 18:20:44 +0700

    🐞fix struct field docs lost on split logic
    remove the logic of appending to previous docs, it only made sense for generics without "pos-inner" docs and anyway was implemented wrongly

--- src/util_strct.rs
+++ src/util_strct.rs
@@ -42,24 +42,13 @@ pub fn extract_doc_fields_shift_up<'a,I>(args: I) -> Result<(Option<Vec<Attribut
           } // ↓ don't set on last item, break before
       }; id_prev = Some(id); // save id even without docs since next docs might need to be split-attached to it
     }
+  } // on ///! split the docs between 2 fields, removing !
+  if        let Some(id_last) = id_last {let (doc2prev,doc2last) = split_doc_in2(docs_last);
+    if ! doc2prev  .is_empty() {doc_fields.push(DocumentedIdent::new(id_prev.expect("saved prev id"), doc2prev  ));} // add last-1 field docs
+    if ! doc2last  .is_empty() {doc_fields.push(DocumentedIdent::new(id_last                        , doc2last  ));} // pos-///! → last field
+  } else if let Some(id_only) = id_only {let (doc2struct,doc2field) = split_doc_in2(docs_last);
+    if ! doc2struct.is_empty() {                                                    doc0struct = Some(doc2struct) ;} // pre-///! → struct
+    if ! doc2field .is_empty() {doc_fields.push(DocumentedIdent::new(id_only                        , doc2field ));} // pos-///! →      field
   }
-  if        let Some(id_last) = id_last { // on ///! split the docs between 2 fields, removing !
-    let (docs2prev,docs2last) = split_doc_in2(docs_last);
-    if ! docs2last.is_empty() {
-      if let Some(mut docum_field_prev) = doc_fields.pop() { // replace last-1 item's docs with its pre-///! docs
-        docum_field_prev.docs = docs2prev;
-        doc_fields.push(docum_field_prev);
-        doc_fields.push(DocumentedIdent::new(id_last, docs2last));
-      } else {                                               // add     last-i item's docs …
-        doc_fields.push(DocumentedIdent::new(id_prev.expect("saved prev ident"), docs2prev));
-        doc_fields.push(DocumentedIdent::new(id_last, docs2last));
-      }
-    }
-  } else if let Some(id_only) = id_only { // on ///! split the docs between struct and field, removing !
-    let (docs2struct,docs2field) = split_doc_in2(docs_last);
-    if ! docs2struct.is_empty() {                docs0struct = Some(docs2struct);}
-    if ! docs2field .is_empty() {
-        doc_fields.push(DocumentedIdent::new(id_only, docs2field));}
-  }
-  Ok((docs0struct,doc_fields))
+  Ok((doc0struct,doc_fields))
 }
