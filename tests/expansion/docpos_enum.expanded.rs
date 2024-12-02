dfb2f25d38151869493230ebb46dfcf39ffed279
eugenesvk <eugenesvk@users.noreply.github.com>
2024-12-02 17:46:34 +0700

    enum: test: update

--- tests/expansion/docpos_enum.expanded.rs
+++ tests/expansion/docpos_enum.expanded.rs
@@ -1,11 +1,11 @@
 use roxygen::*;
-/// enumPre line 1
-/// enumPre line 2
+/// enumPos line1
+/// enumPos line2
 ///
 /// **Variants**:
 ///
 /// * `V1`: v1→v1 line 1
 ///         v1→v1 line 2
 /// * `V2`: v1→v2 line 1
-///         v1→v2 line 2
-pub enum EnumyPos {V1,V2,}
+/// * `V5`: v4→v5 line 1
+pub enum EnumyPos {V1,V2,V3,V4,V5}
--- tests/expansion/docpos_enum.rs
+++ tests/expansion/docpos_enum.rs
@@ -1,25 +1,26 @@
 use roxygen::*;
 
 #[docpos("enum")]
-/// enumPre line 1
-/// enumPre line 2
 pub enum EnumyPos { /// enumPos line1
-  /// enumPos line2
-  V1, /// v1→v1 line 1
-      /// v1→v1 line 2
-      ///! v1→v2 line 1
-      /// v1→v2 line 2
-  V2, //
+                    /// enumPos line2
+  pub V1	,/// v1→v1 line 1
+        	 /// v1→v1 line 2
+  pub V2	,/// v2→v2 line 1
+  pub V3	,// won't be shown
+  pub V4	,// won't be shown
+        	 ///! v4→v5 line 1
+  pub V5	,//
 }
 
+
 #[docpos]
-/// enumPre line 1
-/// enumPre line 2
-pub enum EnumyPos2 { /// enumPos line1
-  /// enumPos line2
-  V1, /// v1→v1 line 1
-      /// v1→v1 line 2
-      ///! v1→v2 line 1
-      /// v1→v2 line 2
-  V2, //
+pub enum EnumyPos { /// enumPos line1
+                    /// enumPos line2
+  pub V1	,/// v1→v1 line 1
+        	 /// v1→v1 line 2
+  pub V2	,/// v2→v2 line 1
+  pub V3	,// won't be shown
+  pub V4	,// won't be shown
+        	 ///! v4→v5 line 1
+  pub V5	,//
 }
