f8aff80831e91e29b5ce8b387c11341a2e3129b1
eugenesvk <eugenesvk@users.noreply.github.com>
2024-12-02 13:45:36 +0700

    test: update struct

--- tests/expansion/docpos_struct.expanded.rs
+++ tests/expansion/docpos_struct.expanded.rs
@@ -7,5 +7,15 @@ use roxygen::*;
 /// * `x`: doc @ x for x
 /// * `y`: doc @ x for y (after `///!`)
 ///
-#[structdocpos]
 pub struct StructyPos {pub x:i8, pub y:i8,}
+
+use roxygen::*;
+/// structPre line 1.
+/// structPre line 2.
+/// scructInner
+///
+/// **Fields**:
+/// * `x`: doc @ x for x
+/// * `y`: doc @ x for y (after `///!`)
+///
+pub struct StructyPos2 {pub x:i8, pub y:i8,}
--- tests/expansion/docpos_struct.rs
+++ tests/expansion/docpos_struct.rs
@@ -1,9 +1,18 @@
 use roxygen::*;
 
-#[structdocpos]
+#[docpos("struct")]
 /// structPre line 1.
 /// structPre line 2.
 pub struct StructyPos { /// scructInner
+  pub x	: i8	,/// doc @ x for x
+       	    	 ///! doc @ x for y (after `///!`)
+  pub y	: i8	,
+}
+
+#[docpos]
+/// structPre line 1.
+/// structPre line 2.
+pub struct StructyPos2 { /// scructInner
   pub x	: i8	,/// doc @ x for x
        	    	 ///! doc @ x for y (after `///!`)
   pub y	: i8	,
