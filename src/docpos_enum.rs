d4900c98d58afd902099c130a319fac6974f951f
eugenesvk <eugenesvk@users.noreply.github.com>
2024-12-03 19:37:46 +0700

    update docpos_enum to work with "extract" arg

--- src/docpos_enum.rs
+++ src/docpos_enum.rs
@@ -3,27 +3,28 @@ use crate::util::make_doc_block;
 use crate::try2 as try2;
 use quote::quote;
 use syn::{Attribute, ItemEnum};
-use crate::util_enum::extract_doc_variants_shift_up;
+use crate::util_enum::{extract_doc_variants_shift_up, doc_variants_shift_up,};
 
 
-/// Document Enum arguments, but after them, not before
-pub fn docpos_enum(mut enm:ItemEnum) -> proc_macro::TokenStream {
+/// Document Enum arguments, but after them, not before. With 'extract' removes the docs from variants to its own smaller section (though the default rustdocs still adds an empty duplicate Variants section)
+pub fn docpos_enum(mut enm:ItemEnum, extract:bool) -> proc_macro::TokenStream {
   try2!(enm.attrs.iter_mut().try_for_each(|attr| {
     if is_docpos_main(attr) {Err(syn::Error::new_spanned(attr,"Duplicate attribute. This attribute must only appear once.",))
     } else                  {Ok(())}}));
 
   let enum_docs = try2!(extract_struct_doc_attrs(&mut enm.attrs)); // extract the doc attributes on the enum itself
 
-  let (doc_params_to_enm, doc_variants) = try2!(extract_doc_variants_shift_up(enm.variants.iter_mut()));
-  let maybe_empty_doc_par_to_enum: Vec<Attribute> = doc_params_to_enm.unwrap_or_else(|| vec![]);
+  let (doc_var2enum, doc_variants) = if extract	{ try2!(extract_doc_variants_shift_up(enm.variants.iter_mut()))
+  } else                                       	{(try2!(        doc_variants_shift_up(enm.variants.iter_mut())), vec![])};
+  let maybe_empty_doc_par_to_enum: Vec<Attribute> = doc_var2enum.unwrap_or_else(|| vec![]);
   // let documented_generics     = try2!(extract_documented_generics_shift_up(&mut enm.generics));
-  let has_doc_variants   = !doc_variants  .is_empty();
-  // let has_documented_generics = !documented_generics.is_empty();
+  let has_doc_variants = !extract || ! doc_variants       .is_empty();
+  // let has_documented_generics =      ! documented_generics.is_empty();
 
-  // if !has_doc_variants && !has_documented_generics {
-  if !has_doc_variants {return syn::Error::new_spanned(enm.ident,"Enum has no documented variants or generics. Document at least one.",).into_compile_error().into();}
+  if !has_doc_variants /*&& !has_documented_generics*/ {
+    return syn::Error::new_spanned(enm.ident,"Enum has no documented variants or generics. Document at least one.",).into_compile_error().into();}
 
-  let parameter_doc_block = make_doc_block("Variants", doc_variants);
+  let parameter_doc_block = if extract{make_doc_block("Variants", doc_variants)} else {None};
   // let generics_doc_block  = make_doc_block("Generics", documented_generics);
 
   let docs_before = enum_docs.before_args_section;
